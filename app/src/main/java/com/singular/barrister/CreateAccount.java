c9bc94f897653f37f1d617733795b3261938afed
rahulbabanaraokalamkar <rahul.kalamkar@hungama.com>
2017-11-21 23:12:41 +0530

    Forgot password New home screen

--- app/src/main/java/com/singular/barrister/CreateAccount.java
+++ app/src/main/java/com/singular/barrister/CreateAccount.java
@@ -1,9 +1,16 @@
 package com.singular.barrister;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class CreateAccount extends AppCompatActivity {
+public class CreateAccount extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView txtPrivacyPolicy;
+    private Button btnCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +21,25 @@ public class CreateAccount extends AppCompatActivity {
         else
             getSupportActionBar().setTitle(getResources().getString(R.string.create_account));
 
+        btnCreate=(Button)findViewById(R.id.textViewRegister);
+        btnCreate.setOnClickListener(this);
+        txtPrivacyPolicy=(TextView)findViewById(R.id.textViewPrivacy);
+        txtPrivacyPolicy.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.textViewPrivacy :
+                Intent intent=new Intent(CreateAccount.this,PrivacyPolicy.class);
+                startActivity(intent);
+                break;
+
+            case R.id.textViewRegister :
+                Intent intent1=new Intent(CreateAccount.this,SignInAccount.class);
+                startActivity(intent1);
+                break;
+        }
     }
 }
--- app/src/main/java/com/singular/barrister/SignInAccount.java
+++ app/src/main/java/com/singular/barrister/SignInAccount.java
@@ -1,9 +1,16 @@
 package com.singular.barrister;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class SignInAccount extends AppCompatActivity {
+public class SignInAccount extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnLogin;
+    private TextView txtCreateNewAccount,txtForgotPassword,txtPrivacyPolicy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +20,40 @@ public class SignInAccount extends AppCompatActivity {
             getActionBar().setTitle(getResources().getString(R.string.sign_in));
         else
             getSupportActionBar().setTitle(getResources().getString(R.string.sign_in));
+
+        btnLogin=(Button)findViewById(R.id.textViewLogin);
+        txtCreateNewAccount=(TextView)findViewById(R.id.textViewNoAccountCreateNew);
+        txtForgotPassword=(TextView)findViewById(R.id.textViewForgotPassword);
+        txtPrivacyPolicy=(TextView)findViewById(R.id.textViewPrivacyPolicy);
+
+        btnLogin.setOnClickListener(this);
+        txtCreateNewAccount.setOnClickListener(this);
+        txtForgotPassword.setOnClickListener(this);
+        txtPrivacyPolicy.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.textViewLogin :
+                Intent intent=new Intent(SignInAccount.this,HomeScreen.class);
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.textViewNoAccountCreateNew :
+                Intent intent1=new Intent(SignInAccount.this,CreateAccount.class);
+                startActivity(intent1);
+                finish();
+                break;
+            case R.id.textViewForgotPassword :
+                Intent intent2=new Intent(SignInAccount.this,ForgotPassword.class);
+                startActivity(intent2);
+                break;
+            case R.id.textViewPrivacyPolicy :
+                Intent  intent3=new Intent(SignInAccount.this,PrivacyPolicy.class);
+                startActivity(intent3);
+                break;
+        }
     }
 }
