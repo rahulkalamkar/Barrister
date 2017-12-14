f748c22e323e5ca4be8d0bc029b46a67cb80dc82
rahul.kalamkar <Manisha@143>
2017-12-14 17:29:51 +0530

    FCM added

--- app/src/main/java/com/singular/barrister/Activity/HomeScreen.java
+++ app/src/main/java/com/singular/barrister/Activity/HomeScreen.java
@@ -6,16 +6,20 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -26,6 +30,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.singular.barrister.Activity.SubActivity.AddCaseActivity;
 import com.singular.barrister.Activity.SubActivity.AddClientActivity;
 import com.singular.barrister.Activity.SubActivity.AddCourtActivity;
@@ -36,6 +41,8 @@ import com.singular.barrister.Fragment.TodaysFragment;
 import com.singular.barrister.Preferance.UserPreferance;
 import com.singular.barrister.R;
 import com.singular.barrister.RetrofitManager.RetrofitManager;
+import com.singular.barrister.Util.FCM.Utils.Config;
+import com.singular.barrister.Util.FCM.Utils.NotificationUtils;
 import com.singular.barrister.Util.NetworkConnection;
 
 import java.lang.reflect.Field;
@@ -52,6 +59,7 @@ public class HomeScreen extends AppCompatActivity {
     ProgressBar progressBar;
 
     FloatingActionButton fab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -86,6 +94,8 @@ public class HomeScreen extends AppCompatActivity {
                 }
             }
         });
+
+        initializeNotification();
         initializeTab();
         initializeFragments();
     }
@@ -357,5 +367,73 @@ public class HomeScreen extends AppCompatActivity {
         } else if (tabLayout.getSelectedTabPosition() == 3) {
             clientFragment.refreshData();
         }
+    }
+
+
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
+
+    public void initializeNotification() {
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                // checking for type intent filter
+                if (intent.getAction().equals(Config.REGISTRATION_COMPLETE)) {
+                    // gcm successfully registered
+                    // now subscribe to `global` topic to receive app wide notifications
+                    FirebaseMessaging.getInstance().subscribeToTopic(Config.TOPIC_GLOBAL);
+
+                    displayFirebaseRegId();
+
+                } else if (intent.getAction().equals(Config.PUSH_NOTIFICATION)) {
+                    // new push notification is received
+
+                    String message = intent.getStringExtra("message");
+
+                    Toast.makeText(getApplicationContext(), "Push notification: " + message, Toast.LENGTH_LONG).show();
+
+                    /*txtMessage.setText(message);*/
+                }
+            }
+        };
+
+        displayFirebaseRegId();
+    }
+
+    // Fetches reg id from shared preferences
+    // and displays on the screen
+    private void displayFirebaseRegId() {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences(Config.SHARED_PREF, 0);
+        String regId = pref.getString("regId", null);
+
+        Log.e("HomeScreen", "Firebase reg id: " + regId);
+
+        if (!TextUtils.isEmpty(regId))
+            Log.e("HomeScreen", "Firebase Reg Id: " + regId);
+        else
+            Log.e("HomeScreen", "Firebase Reg Id is not received yet!");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // register GCM registration complete receiver
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(Config.REGISTRATION_COMPLETE));
+
+        // register new push message receiver
+        // by doing this, the activity will be notified each time a new message arrives
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(Config.PUSH_NOTIFICATION));
+
+        // clear the notification area when the app is opened
+        NotificationUtils.clearNotifications(getApplicationContext());
+    }
+
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
+        super.onPause();
     }
 }
---
+++ app/src/main/java/com/singular/barrister/Util/FCM/Service/BarristerFireBaseInstanceIDService.java
@@ -0,0 +1,51 @@
+package com.singular.barrister.Util.FCM.Service;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.singular.barrister.Util.FCM.Utils.Config;
+
+/**
+ * Created by rahul.kalamkar on 12/14/2017.
+ */
+
+public class BarristerFireBaseInstanceIDService extends FirebaseInstanceIdService {
+
+    String TAG = BarristerFireBaseInstanceIDService.class.getSimpleName();
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        // Saving reg id to shared preferences
+          storeRegIdInPref(refreshedToken);
+
+        Log.e("Token", "" + refreshedToken);
+
+        // sending reg id to your server
+        sendRegistrationToServer(refreshedToken);
+
+        // Notify UI that registration has completed, so the progress indicator can be hidden.
+        Intent registrationComplete = new Intent(Config.REGISTRATION_COMPLETE);
+        registrationComplete.putExtra("token", refreshedToken);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(registrationComplete);
+    }
+
+    private void sendRegistrationToServer(final String token) {
+        // sending gcm token to server
+        Log.e(TAG, "sendRegistrationToServer: " + token);
+    }
+
+    private void storeRegIdInPref(String token) {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences(Config.SHARED_PREF, 0);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("regId", token);
+        editor.commit();
+    }
+}
